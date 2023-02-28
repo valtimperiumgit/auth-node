3e5878831e409ada9870bb8afb54442a40d3d1e2
valtimperiumgit <valtimperium@gmail.com>
2023-02-28 03:08:26 +0200

    Update UserDto.ts

--- src/models/dtos/UserDto.ts
+++ src/models/dtos/UserDto.ts
@@ -1,13 +1,13 @@
 import { IUser } from "../entities/User";
 
 export class UserDto {
-    _id: string;
+    id: string;
     name: string;
     avatar: string;
     email: string;
 
     constructor({_id, name, avatar, email} : IUser){
-        this._id = _id;
+        this.id = _id;
         this.name = name;
         this.avatar = avatar;
         this.email = email;
